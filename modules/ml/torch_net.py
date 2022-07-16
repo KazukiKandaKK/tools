7b906708b043016a07fc558127bccf49cf9fc8d1
Kazuki Kanda <96773362+KazukiKandaKK@users.noreply.github.com>
2022-07-16 10:55:54 +0900

    Develop (#4)
    * Add: NN logic
    
    * Fix: Lint
    
    * Fix: Lint
    
    * Fix: Lint
    
    * Fix: Lint
    
    * Fix: typo
    
    * Fix: Translate
    
    * Fix: yaml
    
    * Fix: yaml

---
+++ modules/ml/torch_net.py
@@ -0,0 +1,37 @@
+'''Pytorch'''
+import torch.nn as nn  # pylint: disable=R0402
+
+
+class Net(nn.Module):  # pylint: disable=R0903
+    '''
+    Create a Neural Network
+    '''
+
+    def __init__(self):
+        super().__init__()
+        self.relu = nn.ReLU()
+        self.pool = nn.MaxPool2d(2, stride=2)
+
+        self.dropout = nn.Dropout(0.2)
+
+        self.conv1 = nn.Conv2d(1, 16, 3)
+        self.conv2 = nn.Conv2d(16, 32, 3)
+
+        self.fc1 = nn.Linear(32 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 10)
+
+    def forward(self, x):  # pylint: disable=C0103
+        '''
+        Create a Forward Logic
+        '''
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.pool(x)
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = self.pool(x)
+        x = x.view(x.size()[0], -1)
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        return x
