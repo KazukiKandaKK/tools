19cac7d4dca21c89f13674508d3ee4e57365256e
kazuki kanda <kazuki.kanda05570@gmail.com>
2022-07-17 08:43:28 +0900

    Add: Mlflow results

--- modules/ml/execute_nn.py
+++ modules/ml/execute_nn.py
@@ -1,10 +1,14 @@
 """Create a Mnist of torch"""
+import pathlib
+import os
+import mlflow
+import yaml
+
 import torch as th
 import torch.nn as nn  # pylint: disable=R0402
 import torch.optim as optim  # pylint: disable=R0402
 import torchvision
 import matplotlib.pyplot as plt
-import mlflow
 
 from tqdm import tqdm
 try:
@@ -20,10 +24,25 @@ class Execute:  # pylint: disable=R0902
     Execute Deep Neural Network
     '''
     # Define
-    BATCH_SIZE = 100
-    WEIGHT_DECAY = 0.005
-    LEARNING_RATE = 0.0001
-    EPOCH = 10
+    PATH = pathlib.Path(os.getcwd())
+    try:
+        with open(fr'{PATH}\items\ml_params.yaml', encoding='utf-8')as f:
+            nn_params = yaml.safe_load(f)
+    except FileNotFoundError:
+        with open(f'{PATH}/items/ml_params.yaml', encoding='utf-8')as f:
+            nn_params = yaml.safe_load(f)
+
+    BATCH_SIZE = nn_params['nn']['batch_size']
+    WEIGHT_DECAY = nn_params['nn']['weight_decay']
+    LEARNING_RATE = nn_params['nn']['lerning_late']
+    EPOCH = nn_params['nn']['epoch']
+
+    params_dict = {
+        'BATCH_SIZE': BATCH_SIZE,
+        'WEIGHT_DECAY': WEIGHT_DECAY,
+        'LEARNING_RATE': LEARNING_RATE,
+        'EPOCH': EPOCH,
+    }
 
     def __init__(self) -> None:
         # Prepare
@@ -35,7 +54,7 @@ class Execute:  # pylint: disable=R0902
         self.trainset = torchvision.datasets.MNIST(
             root='path', train=True, download=True, transform=self.trans)
         self.trainloader = th.utils.data.DataLoader(
-            self.trainset, batch_size=100, shuffle=True, num_workers=2)
+            self.trainset, batch_size=10, shuffle=True, num_workers=2)
         self.testset = torchvision.datasets.MNIST(
             root='~/Documents/work/MNISTDataset/data',
             train=False, download=True, transform=self.trans)
@@ -114,6 +133,19 @@ class Execute:  # pylint: disable=R0902
                 self.test_loss_value.append(
                     sum_loss*self.BATCH_SIZE/len(self.testloader.dataset))
                 self.test_acc_value.append(float(sum_correct/sum_total))
+                result_dict = {
+                    'train_acc_value': self.train_acc_value,
+                    'train_loss_value': self.train_loss_value,
+                    'test_loss_value': self.test_loss_value,
+                    'test_acc_value': self.test_acc_value,
+                }
+                # output for mlflow
+                for key, values in result_dict.items():
+                    for value in values:
+                        mlflow.log_metric(key, value)
+                for key, value in self.params_dict.items():
+                    mlflow.log_param(key, value)
+                mlflow.pytorch.log_model(self.net, 'model')
 
     def print_result(self):
         '''
